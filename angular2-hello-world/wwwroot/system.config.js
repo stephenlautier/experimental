6e0cb651e90167fe5f07160ced5c84e52539693a
Stephen Lautier <stephen.lautier@outlook.com>
2016-01-27 00:26:05 +0100

    refactor(angular2-hello-world): removed redundant dependencies

--- angular2-hello-world/wwwroot/system.config.js
+++ angular2-hello-world/wwwroot/system.config.js
@@ -13,16 +13,13 @@ System.config({
       "deps": [
         "reflect-metadata",
         "es6-shim",
-        "@reactivex/rxjs"
+        "rxjs"
       ]
     }
   },
 
   map: {
-    "@reactivex/rxjs": "npm:@reactivex/rxjs@5.0.0-beta.1",
     "angular2": "npm:angular2@2.0.0-beta.1",
-    "es6-shim": "github:es-shims/es6-shim@0.34.1",
-    "reflect-metadata": "npm:reflect-metadata@0.1.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -55,13 +52,6 @@ System.config({
     },
     "github:jspm/nodelibs-vm@0.1.0": {
       "vm-browserify": "npm:vm-browserify@0.0.4"
-    },
-    "npm:@reactivex/rxjs@5.0.0-beta.1": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "module": "github:jspm/nodelibs-module@0.1.0",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:angular2@2.0.0-beta.1": {
       "crypto": "github:jspm/nodelibs-crypto@0.1.0",
@@ -275,10 +265,6 @@ System.config({
       "string_decoder": "npm:string_decoder@0.10.31"
     },
     "npm:reflect-metadata@0.1.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:reflect-metadata@0.1.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
