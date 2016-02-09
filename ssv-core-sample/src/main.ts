e651bcea1b2d8c508410434b72c3ed3accfa1215
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-09 03:00:46 +0100

    added ssv-core-sample

---
+++ ssv-core-sample/src/main.ts
@@ -0,0 +1,3 @@
+import {utils} from "ssv-core";
+console.log(`>> calc: ${utils.math.add(5,2)}`);
+
---
+++ ssv-core-sample/typings/tsd.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="../wwwroot/jspm_packages/github/sketch7/ssv-core@0.1.4/ssv-core.d.ts" />
